b00a015320b1668287cafd77bb95355ae93a7ccf
Rayhan Ayman <rayhanayman@Rayhans-MacBook-Pro.local>
2024-04-18 16:42:19 +0700

    create: navbar

---
+++ app/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image';
+import './Navbar.css';
+
+const Navbar = () => {
+    const nav = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        color: 'white',
+        height: '80px',
+        padding: '0 20px',
+    };
+
+    return (
+        <div className='nav' style={nav}>
+            <div className="logo">
+                <Image src="/next.svg" alt="logo" style={logo} />
+                {/* <img src="/logo192.png" alt="" /> */}
+            </div>
+            <div className="navList">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
